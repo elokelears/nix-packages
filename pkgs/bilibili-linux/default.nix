7bb5ce57c03ce970e3b9ed2728fa6e76fce45af1
elokelears <elokelears@gmail.com>
2025-03-04 19:10:45 +0800

    update

--- pkgs/bilibili-linux/default.nix
+++ pkgs/bilibili-linux/default.nix
@@ -4,42 +4,36 @@
   electron,
   lib,
   makeWrapper,
+  autoPatchelfHook,
+  libxshmfence,
   ...
 } @ args:
-################################################################################
-# Mostly based on bilibili-bin package from AUR:
-# https://aur.archlinux.org/packages/bilibili-bin
-################################################################################
-stdenv.mkDerivation rec {
-  pname = "bilibili";
-  version = "1.2.1-1";
-  src = fetchurl {
-    url = "https://github.com/msojocs/bilibili-linux/releases/download/v1.2.1-1/io.github.msojocs.bilibili_1.2.1-1_amd64.deb";
-    sha256 = "sha256-t/igezm0ipkOkKION8qTYGK9f6qI3c4iPuS/wWrMywQ=";
-  };
+stdenv.mkDerivation (rec {
+    pname = "bilibili";
+    version = "1.2.1-1";
+    src = fetchurl {
+      url = "https://github.com/msojocs/bilibili-linux/releases/download/v1.2.1-1/io.github.msojocs.bilibili_1.2.1-1_amd64.deb";
+      sha256 = "sha256-t/igezm0ipkOkKION8qTYGK9f6qI3c4iPuS/wWrMywQ=";
+    };
 
-  # 解压 DEB 包
-  unpackPhase = ''
-    ar x ${src}
-    tar xf data.tar.xz
-  '';
+    nativeBuildInputs = [makeWrapper autoPatchelfHook];
+    buildInputs = [libxshmfence];
 
-  # makeWrapper 可以自动生成一个调用其它命令的命令（也就是 wrapper），并且可以在原命令上修改参数、环境变量等
-  buildInputs = [makeWrapper];
+    unpackPhase = ''
+      ar x ${src}
+      tar xf data.tar.xz
+    '';
 
-  installPhase = ''
-    mkdir -p $out/bin
+    installPhase = ''
+      mkdir -p $out/{bin,opt}
+      cp -r usr/share $out/share
+      cp -r opt/apps/io.github.msojocs.bilibili/files/bin/app.asar $out/opt
 
-    # 替换菜单项目（desktop 文件）中的路径
-    cp -r usr/share $out/share
-    sed -i "s|Exec=.*|Exec=$out/bin/bilibili|" $out/share/applications/*.desktop
+      sed -i "s|/opt/apps/.*/files/bin|$out/bin|g" $out/share/applications/*.desktop
 
-    # 复制出客户端的 Javascript 部分，其它的不要了
-    cp -r opt/apps/io.github.msojocs.bilibili/files/bin/app $out/opt
-
-    # 生成 bilibili 命令，运行这个命令时会调用 electron 加载客户端的 Javascript 包（$out/opt/app.asar）
-    makeWrapper ${electron}/bin/electron $out/bin/bilibili \
-      --argv0 "bilibili" \
-      --add-flags "$out/opt/app.asar"
-  '';
-}
+      makeWrapper ${electron}/bin/electron $out/bin/bilibili \
+        --argv0 "bilibili" \
+        --add-flags "$out/opt/app.asar"
+    '';
+  }
+  // args)
